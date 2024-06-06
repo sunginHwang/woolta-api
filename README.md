cc18111702edd04b75f3395bafe9996445c55161
sunginHwang <gommpo111@gmail.com>
2024-06-06 21:43:17 +0900

    feat: woolbank schema prisma 정의

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## DB 싱크
 > npx prisma generate --schema=prisma/{schema_name}.prisma
+
+## graphql-codegen
+> npm run codegenAll # graphql-codegen을 통해 typescript type을 생성합니다.
--- codegenWoolBank.ts
+++ codegenWoolBank.ts
@@ -5,7 +5,7 @@ import { defineConfig } from '@eddeee888/gcg-typescript-resolver-files'
 
 const config: CodegenConfig = {
     overwrite: true,
-    schema:"src/apps/woolBank/schema/**/schema.graphql",
+    schema:"src/apps/woolBank/schema/schema.graphql",
     generates: {
         'src/apps/woolBank/generates': defineConfig(),
     }
